1382d9370d476a1ba8ac5cc5daa7931ec9e91242
Grzeni <grzwini74@gmail.com>
2021-09-14 17:31:36 +0200

    fixed copying invite bug

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://quizzical-borg-ced3aa.netlify.app"
     }
 });
 
